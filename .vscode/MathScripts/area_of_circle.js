45d048637e4e595595a8ef35bbbf856ad899742d
yuvaranik <yuvaranikv@gmail.com>
2024-05-06 12:36:43 +0530

    Added tips and area of circle files

---
+++ .vscode/MathScripts/area_of_circle.js
@@ -0,0 +1,7 @@
+//To calculate Area of circle for radius 6
+const pi=3.14159;
+
+let radius=6;
+let area=pi*6*6;
+console.log("The area of a circle with radius " + `${radius}` + " is" +` `+ area);
+
